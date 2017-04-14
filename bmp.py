e2e022ced75cccedb0e134d2f1ed93f1790c118b
evilPaprika <ivil.paprika@gmail.com>
2017-04-14 16:24:06 +0500

    заготовки функций

--- bmp.py
+++ bmp.py
@@ -1,1 +1,16 @@
 
+
+def get_bitmap_file_header(file):
+    pass
+
+def get_bitmap_info_header(file):
+    pass
+
+def get_bmp_version(bitmap_file_header):
+    pass
+
+def get_width_and_height(bitmap_info_header):
+    pass
+
+if __name__ == '__main__':
+    pass
